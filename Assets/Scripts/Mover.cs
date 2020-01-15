c61d2130412455f16fdb0e3e992a25da48fda61b
Matthew Berger <mb185428@ncr.com>
2020-01-15 16:01:33 -0500

    Setting up git ignore and git attributes for large files

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    private NavMeshAgent _navMeshAgent;
+
+    private void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        _navMeshAgent.destination = target.position;
+    }
+}
